57ca8cb764a7d88e3e15e4272af59fbdd5b4142f
Lakshminiwash <lakshminiwash07rr@gmail.com>
2026-05-25 09:22:14 +0530

    authentication completed

--- backend/src/router/auth.router.js
+++ backend/src/router/auth.router.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { register } from "../controller/auth.controller.js";
-import { registerValidator } from "../validator/auth.validator.js";
+import { register,verify,login, getMe } from "../controller/auth.controller.js";
+import { registerValidator,loginValidator } from "../validator/auth.validator.js";
+import { authUser } from "../middleware/auth.middleware.js";
 
 const authRouter = Router()
-
+// /api/auth/register 
 authRouter.post("/register",registerValidator,register)
+
+// /api/auth/login 
+authRouter.post("/login",loginValidator,login)
+
+// /api/auth/getme
+authRouter.get("/getme",authUser,getMe)
+
+// /api/auth/verify-email 
+authRouter.get("/verify-email",verify)
 
 export default authRouter
