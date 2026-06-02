3d377f7c34b0d1e726ffda68decfb41fdb5020d2
Lakshminiwash <lakshminiwash07rr@gmail.com>
2026-06-03 01:37:24 +0530

    --

--- backend/src/service/mail.service.js
+++ backend/src/service/mail.service.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
+    port: "587",
     secure: false,
     auth: {
         user: process.env.BREVO_USER?.trim(),
