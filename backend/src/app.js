00d90909741cfd8dfe5cf5a526d65d00798405b1
Lakshminiwash <lakshminiwash07rr@gmail.com>
2026-06-02 22:34:21 +0530

    fix the bugs

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import cors from "cors";
 import chatRouter from "./router/chatRoutes.js";
+import cron from "node-cron"
+import axios from "axios"
 
 const app = express();
 
@@ -17,9 +19,23 @@ app.use(cors({
     methods:["GET","POST","PUT","DELETE"],
 }))
 
+// ✅ Health check route
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "ok" })
+})
 
 
 app.use("/api/auth",authRouter)
 app.use("/api/chat",chatRouter)
+
+// ✅ Cron job
+cron.schedule("*/14 * * * *", async () => {
+    try {
+        await axios.get("https://perplexity-backend-8zm6.onrender.com/api/health")
+        console.log("Server pinged - staying awake")
+    } catch (error) {
+        console.log("Ping failed")
+    }
+})
 
 export default app
