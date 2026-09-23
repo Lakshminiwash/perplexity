a3ecbcf867dbb63b79660c4a988f77e80fdad0b5
Lakshminiwash <lakshminiwash07rr@gmail.com>
2026-09-23 10:07:36 +0530

    fix the configurations

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ import axios from "axios"
 
 // CORS middleware configuration
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://perplexity-kappa-brown.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
--- frontend/src/features/chat/services/chat.api.js
+++ frontend/src/features/chat/services/chat.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"http://localhost:3000/",
+    baseURL:"https://perplexity-4d0u.onrender.com/",
     withCredentials:true
 })
 
